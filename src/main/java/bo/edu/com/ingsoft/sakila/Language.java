8b281d5c8707e437d1e7904855333ea6daffacfe
Mire11 <mireyahuanca3@gmail.com>
2021-10-05 03:37:48 -0400

    primer_commit

---
+++ src/main/java/bo/edu/com/ingsoft/sakila/Language.java
@@ -0,0 +1,50 @@
+package bo.edu.com.ingsoft.sakila;
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity(name="language")
+public class Language {
+    @Id
+    @Column(name="language_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer language_id;
+    
+    private String name;
+    //@Column(name="name")
+    private Date last_update;
+    //@Column(name="last_update")
+
+
+    public Language () {
+    }
+
+    public Integer getLanguage_id() {
+        return language_id;
+    }
+
+    public void setLanguage_id(Integer language_id) {
+        this.language_id = language_id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getLast_update() {
+        return last_update;
+    }
+
+    public void setLast_update(Date last_update) {
+        this.last_update = last_update;
+    }
+    
+}
